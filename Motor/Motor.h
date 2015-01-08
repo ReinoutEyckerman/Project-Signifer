075f095a59340446e96d1d3ab15d674bac6ac626
Oliver <Oli.hofkens@gmail.com>
2015-01-08 14:57:35 +0100

    Added overloads to Motor library, commented Motor library.

--- Motor/Motor.h
+++ Motor/Motor.h
@@ -1,14 +1,22 @@
+/*
+Motor.h - Library for controlling a motor through the L293D IC.
+Created by Oliver Hofkens, November 2014.
+Released into the public domain.
+*/
 #ifndef Motor_h
 #define Motor_h
 
 #include "Arduino.h"
 
+//See .cpp file for comments on the functions.
 class Motor
 {
   public:
     Motor(int pinForward, int pinBackward);
     void DriveForward(unsigned int power);
+	void DriveForward();
     void DriveBackward(unsigned int power);
+	void DriveBackward();
     void Stop();   
   private:
    int _pinForward;
