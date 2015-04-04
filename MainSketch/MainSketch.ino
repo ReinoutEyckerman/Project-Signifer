b1fb79e4d0b7a7980705df9f7d76069ef6917682
Reinout <reinouteyckerman@gmail.com>
2015-04-04 11:45:18 +0200

    FIST tweaking, the ability to pause/play music arduino

--- MainSketch/MainSketch.ino
+++ MainSketch/MainSketch.ino
@@ -1,21 +1,34 @@
 #include <Motor.h>
 #include <TwoWheelDrive.h>
 
-const int LeftDriverForward = 3;
-const int LeftDriverBackward = 5;
+const int LeftDriverForward = 5;
+const int LeftDriverBackward = 6;
 const int RightDriverForward = 9;
 const int RightDriverBackward = 10;
+int test=0;
 
 TwoWheelDrive Driver(LeftDriverForward,LeftDriverBackward,RightDriverForward,RightDriverBackward);
 
 int index = 0;
-
+const int awakePin=3;
+const int sleepPin=2;
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+ pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+ digitalWrite(2,LOW); 
+ digitalWrite(3,LOW); 
 
 }
 
 void loop() {
+//  for(;;){
+ // if (Serial.available())
+  //  Serial.write(3);
+ // }
+  digitalWrite(awakePin,LOW); 
+ digitalWrite(sleepPin,LOW); 
+
   if (index % 5 == 0) {
     Driver.Forward();
   }
@@ -35,5 +48,23 @@ void loop() {
     Driver.PivotRight();
   }
   index++;
-  delay(3000);
+  delay(10000);
+  if(test==0)
+  {
+    test=1;
+    Serial.println("test");
+    Awake();
+    }
+   else{
+   test=0;
+       Serial.println("test1");
+   Sleep();
+   }
 }
+void Awake(){
+ digitalWrite(awakePin,HIGH); 
+}
+void Sleep(){  
+  delay(100);
+  digitalWrite(sleepPin, HIGH);
+  }
