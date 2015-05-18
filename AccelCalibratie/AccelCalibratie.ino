a5ccc14668c05a1d1ceeaa64adf425b097cd3300
OliverHofkens <oli.hofkens@gmail.com>
2015-05-18 16:22:09 +0200

    Calibraties gecalibreerd, hoera teamwork.

--- AccelCalibratie/AccelCalibratie.ino
+++ AccelCalibratie/AccelCalibratie.ino
@@ -7,8 +7,18 @@ int X2 = A3;
 int Y2 = A4;
 int Z2 = A5;
 
-//RESULTS 18-05-2015:
-// 
+//RESULTS 18-05-2015 RECHTSE HAND
+// PLAT:           1 0 0 1 0 0   
+// KANTEL RECHTS:  0 0 0 0 0 0  
+// ONDERSTEBOVEN:  0 1 1 0 0 1
+// OMHOOG:         0 -1 1 -1 -1 -1
+// BENEDEN:        0 0 1 0 1 1
+
+// FIST PLAT:           1 0 0 0 1 1
+// FIST KANTEL RECHTS:  0 0 0 0 0 0
+// FIST ONDERSTEBOVEN:  0 0 1 0 0 1
+// FIST OMHOOG:         0 -1 0 1 0 0
+// FIST BENEDEN:        0 1 1 0 1 1
 
 void setup() {
   Serial.begin(9600);
@@ -22,7 +32,14 @@ void loop() {
   int m4 = analogRead(X2);
   int m5 = analogRead(Y2);
   int m6 = analogRead(Z2);
-  
+
+  int c1 = AcceleroConvert(m1);
+  int c2 = AcceleroConvert(m2);
+  int c3 = AcceleroConvert(m3);
+  int c4 = AcceleroConvert(m4);
+  int c5 = AcceleroConvert(m5);
+  int c6 = AcceleroConvert(m6);
+
   Serial.print("1:");
   Serial.print(" X: ");
   Serial.print(m1);
@@ -30,15 +47,46 @@ void loop() {
   Serial.print(m2);
   Serial.print(" Z: ");
   Serial.print(m3);
-  
+
   Serial.print(" | 2: ");
   Serial.print(" X: ");
   Serial.print(m4);
   Serial.print(" Y: ");
   Serial.print(m5);
   Serial.print(" Z: ");
-  Serial.println(m6);
-  
+  Serial.print(m6);
+
+
+
+
+
+  Serial.print(" | C1:");
+  Serial.print(" X: ");
+  Serial.print(c1);
+  Serial.print(" Y: ");
+  Serial.print(c2);
+  Serial.print(" Z: ");
+  Serial.print(c3);
+
+  Serial.print(" | C2: ");
+  Serial.print(" X: ");
+  Serial.print(c4);
+  Serial.print(" Y: ");
+  Serial.print(c5);
+  Serial.print(" Z: ");
+  Serial.println(c6);
+
   delay(1000);
 
 }
+
+int AcceleroConvert(int c) {
+  if (c < 285)
+    return -1;
+  else if (c > 285 && c < 385)
+    return 0;
+  else if (c > 385)
+    return 1;
+  else return 2;
+}
+
